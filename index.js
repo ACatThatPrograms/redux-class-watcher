a78ed28ba181451ce1bbd07014b1a78191b362cd
ACatThatPrograms <25625537+ACatThatPrograms@users.noreply.github.com>
2022-04-21 13:35:03 -0400

    Use inspect over stringify

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 import { createSlice, current } from '@reduxjs/toolkit';
+import { inspect } from 'util';
 
 export function classInstanceReducer(classInstance, sliceName) {
     // Note each key, and build initial state from them
     let initialState = {};
     Object.keys(classInstance).forEach(key => {
-        if (typeof classInstance[key] === 'function' || isClass(classInstance[key])) {
-            // Do nothing. . .
+        if (typeof classInstance[key] === "undefined") {
+            console.warn(`Undefined key in class instancee looking to track: ${key}. Ignore if expected. If not make sure it is populated prior to constructing the reducer.`);
         } else {
-            initialState[key] = classInstance[key];
+            initialState[key] = inspect(classInstance[key]);
         }
     })
-    console.log(initialState)
     // Create the redux slice
     const classSlice = createSlice({
         name: sliceName || classInstance.constructor.name,
@@ -19,23 +19,11 @@ export function classInstanceReducer(classInstance, sliceName) {
             equalize: function (state, action) {
                 let currentState = current(state);
                 Object.keys(currentState).forEach(stateKey => {
-                    state[stateKey] = classInstance[stateKey];
+                    state[stateKey] = inspect(classInstance[stateKey]);
                 })
             }
         },
     })
     // Return the reducer and the state equalize action for dispatching
     return [classSlice.reducer, classSlice.actions.equalize];
-}
-
-function isClass(obj) {
-    const isCtorClass = obj.constructor
-        && obj.constructor.toString().substring(0, 5) === 'class'
-    if (obj.prototype === undefined) {
-        return isCtorClass
-    }
-    const isPrototypeCtorClass = obj.prototype.constructor
-        && obj.prototype.constructor.toString
-        && obj.prototype.constructor.toString().substring(0, 5) === 'class'
-    return isCtorClass || isPrototypeCtorClass
 }
